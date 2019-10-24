ed98a734c1845fa8e5b16aff9caa011940aa6e57
Elucidatorsky <467845371@qq.com>
2019-10-24 22:45:27 +0800

    new

---
+++ algorithm/stairs/zimu.js
@@ -0,0 +1,16 @@
+const w = new Map(); // 数据结构 Hash表
+var zimu = []
+function f(n) {
+    // 归 退出条件
+    if(n == 1)  return 1;
+    if(n == 2)  return 2;
+    if(w.has(n)){
+        return w.get(n);
+    }
+    var ret = n * f(n-1) ;
+    w.set(n, ret);
+    return ret; // 递
+}
+
+f(26);
+console.log(w);
---
+++ snail/koa-notebook/noteBook/controllers/defaultConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    database: {
+        DATABASE: 'test',
+        USERNAME: 'root',
+        PASSWORD: 'password',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
